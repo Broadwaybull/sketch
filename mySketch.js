e23a5640e0ad960d04bf6fd5bff380bbf1693b40
Broadwaybull <64816175+Broadwaybull@users.noreply.github.com>
2021-09-07 14:40:23 -0400

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -9,7 +9,7 @@ var button;
 let img;
 
 function setup() {
-	canvas = createCanvas(500, 500);
+	canvas = createCanvas(500, 300);
 	background(100);
 	h1 = createElement("h1","i am h1");
 	canvas.position(0,0);
